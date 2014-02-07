4776d08babae9e0d2f33ac34ab0cdc93a2679d5a
aseifu <abiy.seifu@nitropdf.com>
2014-02-07 09:58:56 -0800

    yeoman scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('rippleTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
