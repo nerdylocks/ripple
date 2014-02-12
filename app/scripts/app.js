831433ed778d9d9a35d1ce5885529cc21201e56d
aseifu <abiy.seifu@nitropdf.com>
2014-02-12 06:07:44 -0800

    Currency Snap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,13 @@
 'use strict';
 
-angular.module('rippleTestApp', [
-'ngCookies',
-'ngResource',
-'ngSanitize',
-'ngRoute',
-'angular-websocket',
-'controllers'
-])
-.config(function(WebSocketProvider){
-	WebSocketProvider
-	.prefix('')
-	.uri('wss://s1.ripple.com');
-})
-.config(function ($routeProvider) {
+var rippleApp = angular.module('rippleTestApp', [
+	'ngCookies',
+	'ngResource',
+	'ngSanitize',
+	'ngRoute'
+]);
+
+rippleApp.config(function ($routeProvider) {
 	$routeProvider
 	.when('/', {
 		templateUrl: 'views/main.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,31 @@
 'use strict';
 
-angular.module('controllers', [])
-.controller('MainCtrl', function ($scope, WebSocket) {
-	WebSocket.onopen(function(){
-		console.log('connection');
-		WebSocket.send('{"command":"subscribe","id":0,"streams":["ledger"]}');
-	});
-	$scope.socketResp = [];
-	WebSocket.onmessage(function(event){
-		$scope.socketResp.push(JSON.parse(event.data));
-	});
+rippleApp.controller('MainCtrl', function ($scope, $rootScope, RippleRemote) {
+	
+	RippleRemote.init();		
+
+	$scope.connect = function(){
+		$rootScope.$emit('CONNECTING');
+		RippleRemote.on('transaction_all', handleData);
+	}
+	
+	function handleData(data){
+		$rootScope.$apply(function(){
+			$rootScope.$emit('CONNECTED');
+			if(data.engine_result_code == 0){
+				if(data.transaction.hasOwnProperty("TakerPays")) {
+					if(data.transaction.TakerPays.hasOwnProperty("currency")){
+						var _counter = $rootScope.currencyTypes[data.transaction.TakerPays.currency] || $rootScope.currencyTypes['other'];
+						$rootScope.currencyTypes[_counter.currency].amount = $rootScope.currencyTypes[_counter.currency].amount+1;
+					}
+				} else if(data.transaction.hasOwnProperty("TakerGets")) {
+					if(data.transaction.TakerPays.hasOwnProperty("currency")){
+						var _counter = $rootScope.currencyTypes[data.transaction.TakerGets.currency] || $rootScope.currencyTypes['other'];
+						$rootScope.currencyTypes[_counter.currency].amount = $rootScope.currencyTypes[_counter.currency].amount+1;
+					}
+				}	
+			}
+		});
+	}
+
 });
